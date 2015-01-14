72843d8d295ac4f5d4a00d14034d463e4a56f758
熊辉 <jackxiong@sohu-inc.com>
2015-01-14 17:20:15 +0800

    修改util类

--- src/main/java/org/opensource/clearpool/util/ThreadSleepUtil.java
+++ src/main/java/org/opensource/clearpool/util/ThreadSleepUtil.java
@@ -2,7 +2,7 @@ package org.opensource.clearpool.util;
 
 /**
  * This class's duty is to release CPU,it sleep for a while in case the CPU is full running.
- * 
+ *
  * @author xionghui
  * @date 26.07.2014
  * @version 1.0
@@ -14,8 +14,7 @@ public class ThreadSleepUtil {
    */
   public static void sleep() {
     try {
-      // Thread.sleep(0, 1);
-      Thread.sleep(0);
+      Thread.sleep(0, 1);
     } catch (InterruptedException e) {
       // swallow the exception
     }
